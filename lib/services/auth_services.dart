c952f0b129bd58f52256e048a9ba36341ec5788f
Abirami S <abirami.s77@gmail.com>
2019-07-26 10:01:28 -0700

    Google Sign In

--- lib/services/auth_services.dart
+++ lib/services/auth_services.dart
@@ -6,30 +6,18 @@ class AuthService {
   // Login
   Future<bool> login() async {
     // Simulate a future for response after 2 second.
-<<<<<<< HEAD
     return await Future<bool>.delayed(
         Duration(
-            seconds: 2
+            seconds: 1
         ), () => Random().nextBool()
-=======
-    return await new Future<bool>.delayed(
-        new Duration(
-            seconds: 2
-        ), () => new Random().nextBool()
->>>>>>> origin/wip
     );
   }
 
   // Logout
   Future<void> logout() async {
     // Simulate a future for response after 1 second.
-<<<<<<< HEAD
     return await Future<void>.delayed(
         Duration(
-=======
-    return await new Future<void>.delayed(
-        new Duration(
->>>>>>> origin/wip
             seconds: 1
         )
     );
